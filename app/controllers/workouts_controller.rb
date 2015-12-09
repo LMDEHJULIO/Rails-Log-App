729608dced36ca1db20a9919e16a767673f0daa3
Julio Rodriguez <Julio@Julios-MacBook-Pro.local>
2015-12-08 23:16:57 -0500

    form added

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -2,18 +2,37 @@ class WorkoutsController < ApplicationController
 	def index
 	end 
 
+	def show 
+	end
+
 	def new
+		@workout = Workout.new
 	end 
 
 	def create
-	end 
-
-	def update
+		@workout = Workout.new(workout_params)
+		if @workout.save
+			redirect_to @workout
+		else 
+			render 'new'
+		end 
 	end 
 
 	def edit
 	end 
 
+	def update
+	end 
+
+
+
 	def destroy
+	end 
+
+	def workout_params
+		params.require(:workout).permit(:date, :workout, :mood, :length)
+	end 
+
+	def find_workout 
 	end 
 end
